ec71e14287bcbfd6fd373eccdfc8f9eeda0a9c51
santmartines <129224648+santmartines@users.noreply.github.com>
2023-10-30 22:54:40 -0600

    Add files via upload

---
+++ 5IV6_Martinez-Ramirez-Santiago_SWyA/01 CESAR/cesar.js
@@ -0,0 +1,28 @@
+const desplazamiento = document.getElementById("desplazamiento");
+const texto = document.getElementById("texto");
+const textoCifrado = document.getElementById("cifrado");
+
+//vamos a crear uina funcion para cifrar
+function cifrado(){
+    //obtener el texto que se ingresa
+    const textoIngresado = texto.value;
+    //obtener caracter por caracter y validar la entrada del texto
+    textoCifrado.value = textoIngresado.split('').map(c => { 
+        let mayus = (c === c.toUpperCase()) ? true : false;
+        let valorEntero = c.toLowerCase().charCodeAt(0);
+        if(valorEntero >= 97 && valorEntero <= 122){
+            const valorDesplazamiento = parseInt(desplazamiento.value);
+            if(valorEntero + valorDesplazamiento > 122){
+                valorEntero = 97 + (valorEntero - 122) + valorDesplazamiento - 1;
+            }else{
+                valorEntero = valorEntero + valorDesplazamiento;
+            }
+
+        }
+        let cifrado = String.fromCharCode(valorEntero);
+        return mayus ? cifrado.toUpperCase() : cifrado;
+    }).join('');
+}
+
+texto.addEventListener("keyup", cifrado);
+desplazamiento.addEventListener("change", cifrado);
